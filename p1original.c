960ff98d8d7f45fc3970b6f500c32e59b5a99d0e
MayurS <83947881+GorillaEaT@users.noreply.github.com>
2022-02-23 04:47:24 +0000

    1

--- p1original.c
+++ p1original.c
@@ -1,32 +1,30 @@
 #include<stdio.h>
 #include<math.h>
-void cord1(int *x1,int *y1)
+void cord1(float *x1,float *y1)
 {
     printf("Enter the 1st coordinates:\n");
-    scanf("%d%d",x1,y1);
+    scanf("%f%f",x1,y1);
 }
-void cord2(int *x2,int *y2)
+void cord2(float *x2,float *y2)
 {
     printf("Enter the 2st coordinates:\n");
-    scanf("%d%d",x2,y2);
+    scanf("%f%f",x2,y2);
 }
-float compute(int x1,int y1,int x2,int y2)
+void compute(float x1,float y1,float x2,float y2,float *dis)
 {
-    float dis=0.0;
-    dis=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
-    return dis;
+  *dis=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
 }
-void output(int x1,int y1,int x2,int y2,float r)
+void output(float x1,float y1,float x2,float y2,float dis)
 {
-    printf("the distance between (%d,%d) and (%d,%d) is %f\n",x1,y1,x2,y2,r);
+    printf("the distance between (%f,%f) and (%f,%f) is %f\n",x1,y1,x2,y2,dis);
 }
 int main()
 {
-    int x1,y1,x2,y2;
-    float r;
+    float x1,y1,x2,y2;
+    float dis;
     cord1(&x1,&y1);
     cord2(&x2,&y2);
-    r=compute(x1,y1,x2,y2);
-    output(x1,y1,x2,y2,r);
+    compute(x1,y1,x2,y2,&dis);
+    output(x1,y1,x2,y2,dis);
     return 0;
 }
